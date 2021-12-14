0636499db57b95c86fbb1f99276a9a27c7cd9b8e
emdzej <jurcak.matej@gmail.com>
2021-12-14 12:59:36 +0100

    Add script to insert initial values

--- src/database/sql/insert_initial_values.sql
+++ src/database/sql/insert_initial_values.sql
@@ -1,29 +1,55 @@
-INSERT INTO stop VALUES (1, 'S-A');
-INSERT INTO stop VALUES (2, 'S-B');
-INSERT INTO stop VALUES (3, 'S-C');
-INSERT INTO stop VALUES (4, 'S-D');
-INSERT INTO stop VALUES (5, 'S-E');
-INSERT INTO stop VALUES (6, 'S-F');
+INSERT INTO stop
+VALUES (1, 'S-A');
+INSERT INTO stop
+VALUES (2, 'S-B');
+INSERT INTO stop
+VALUES (3, 'S-C');
+INSERT INTO stop
+VALUES (4, 'S-D');
+INSERT INTO stop
+VALUES (5, 'S-E');
+INSERT INTO stop
+VALUES (6, 'S-F');
 
-INSERT INTO line VALUES(1, 'L-A', 1);
-INSERT INTO line VALUES(2, 'L-B', 1);
-INSERT INTO line VALUES(3, 'L-C', 2);
-INSERT INTO line VALUES(4, 'L-D', 2);
-INSERT INTO line VALUES(5, 'L-E', 3);
-INSERT INTO line VALUES(6, 'L-F', 3);
+INSERT INTO line
+VALUES (1, 'L-A', 1);
+INSERT INTO line
+VALUES (2, 'L-B', 1);
+INSERT INTO line
+VALUES (3, 'L-C', 2);
+INSERT INTO line
+VALUES (4, 'L-D', 2);
+INSERT INTO line
+VALUES (5, 'L-E', 3);
+INSERT INTO line
+VALUES (6, 'L-F', 3);
 
-INSERT INTO line_stop VALUES(1, 1, 1);
-INSERT INTO line_stop VALUES(2, 1, 2);
-INSERT INTO line_stop VALUES(3, 1, 3);
-INSERT INTO line_stop VALUES(4, 1, 4);
-INSERT INTO line_stop VALUES(5, 2, 2);
-INSERT INTO line_stop VALUES(6, 2, 3);
-INSERT INTO line_stop VALUES(7, 2, 4);
-INSERT INTO line_stop VALUES(8, 2, 5);
-INSERT INTO line_stop VALUES(9, 2, 6);
-INSERT INTO line_stop VALUES(10, 3, 1);
-INSERT INTO line_stop VALUES(11, 3, 2);
-INSERT INTO line_stop VALUES(12, 3, 3);
-INSERT INTO line_stop VALUES(13, 3, 4);
-INSERT INTO line_stop VALUES(14, 4, 5);
+INSERT INTO line_stop
+VALUES (1, 1, 1);
+INSERT INTO line_stop
+VALUES (2, 1, 2);
+INSERT INTO line_stop
+VALUES (3, 1, 3);
+INSERT INTO line_stop
+VALUES (4, 1, 4);
+INSERT INTO line_stop
+VALUES (5, 2, 2);
+INSERT INTO line_stop
+VALUES (6, 2, 3);
+INSERT INTO line_stop
+VALUES (7, 2, 4);
+INSERT INTO line_stop
+VALUES (8, 2, 5);
+INSERT INTO line_stop
+VALUES (9, 2, 6);
+INSERT INTO line_stop
+VALUES (10, 3, 1);
+INSERT INTO line_stop
+VALUES (11, 3, 2);
+INSERT INTO line_stop
+VALUES (12, 3, 3);
+INSERT INTO line_stop
+VALUES (13, 3, 4);
+INSERT INTO line_stop
+VALUES (14, 4, 5);
 
